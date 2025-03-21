f21a67f46cca228f34f822b381d778be49232e6f
Feder1414 <f.bogota@uniandes.edu.co>
2025-03-21 13:14:04 -0500

    - Added proximity notifications
    - Now when a user is near a sport event, they will receive a notification
    - The notification will contain the name of the event and the distance to it
    - Leaved placeholder for implement attendance notifications

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -49,6 +50,7 @@ flutter {
 dependencies {
   // Import the Firebase BoM
   implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
+  
 
 
   // TODO: Add the dependencies for Firebase products you want to use
@@ -58,4 +60,6 @@ dependencies {
 
   // Add the dependencies for any other desired Firebase products
   // https://firebase.google.com/docs/android/setup#available-libraries
+        coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+
 }
